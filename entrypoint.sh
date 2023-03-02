a64b40d99a5630fe1ed76468330a3d0592e0e902
Kihong Heo <kihong.heo@kaist.ac.kr>
2023-03-02 16:14:01 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ls -al
+whoami
 eval $(opam env)
 # $1: issue_num, $2: issue_contents, $3: repository_path_name, $4: rapid_key, $5: repo_key, $6: threshold
 dune exec ./dup_scan.exe $1 "contents:$2" $3 $4 "$5" "$6"
