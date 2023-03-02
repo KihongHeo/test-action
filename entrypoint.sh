64cc9e10c3809f58fb0870fc62211a3299703fa4
Kihong Heo <kihong.heo@kaist.ac.kr>
2023-03-02 16:03:55 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-#dune exec ./dup_scan.exe $1 "contents:$2" $3 $4 "$5" "$6" # $1: issue_num, $2: issue_contents, $3: repository_path_name, $4: rapid_key, $5: repo_key, $6: threshold
-dune exec ./dup_scan.exe 1
+eval $(opam env)
+# $1: issue_num, $2: issue_contents, $3: repository_path_name, $4: rapid_key, $5: repo_key, $6: threshold
+dune exec ./dup_scan.exe $1 "contents:$2" $3 $4 "$5" "$6"
